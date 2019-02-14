ff08322f80ca4395efbdd2f7a5dba7c509956505
Jens Eickmeyer <jens.eickmeyer@accenture.com>
2019-02-14 09:09:06 +0100

    Use Swagger to configure the API Gateway

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,9 @@ OUTPUT_FILE=sam-template-output.yaml
 STAGE_NAME=dev
 STACK_NAME=todo-app-$STAGE_NAME
 
+# Copy Swagger file to S3 to be able to transform its content for "deploying" it to API Gateway
+aws s3 cp swagger.yaml s3://$S3_BUCKET/swagger.yaml
+
 cd src
 npm install
 npm run-script lint
@@ -14,7 +17,7 @@ npm prune --production
 cd ..
 
 aws cloudformation package --template-file $INPUT_FILE --output-template-file $OUTPUT_FILE --s3-bucket $S3_BUCKET
-aws cloudformation deploy --template-file $OUTPUT_FILE --stack-name $STACK_NAME --parameter-overrides StageName=$STAGE_NAME --capabilities CAPABILITY_IAM
+aws cloudformation deploy --template-file $OUTPUT_FILE --stack-name $STACK_NAME --parameter-overrides StageName=$STAGE_NAME S3BucketName=$S3_BUCKET --capabilities CAPABILITY_IAM
 
 export API_GATEWAY_URL=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[0].OutputValue' --output text)
 
