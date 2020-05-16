88b119328787b898500fe39f44369939432741ea
Jens Eickmeyer <jens.eickmeyer@carpinuslabs.com>
2020-05-16 09:53:23 +0200

    Use „npm ci“ instead of „npm install“

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 S3_BUCKET=
 INPUT_FILE=sam-template.yaml
 OUTPUT_FILE=sam-template-output.yaml
@@ -11,13 +10,18 @@ STACK_NAME=todo-app-$STAGE_NAME
 aws s3 cp swagger.yaml s3://$S3_BUCKET/swagger.yaml
 
 cd src
-npm install
+npm ci
 npm run-script lint
 npm prune --production
 cd ..
 
-aws cloudformation package --template-file $INPUT_FILE --output-template-file $OUTPUT_FILE --s3-bucket $S3_BUCKET
-aws cloudformation deploy --template-file $OUTPUT_FILE --stack-name $STACK_NAME --parameter-overrides StageName=$STAGE_NAME S3BucketName=$S3_BUCKET --capabilities CAPABILITY_IAM
+aws cloudformation package --template-file $INPUT_FILE \
+                           --output-template-file $OUTPUT_FILE \
+                           --s3-bucket $S3_BUCKET
+aws cloudformation deploy --template-file $OUTPUT_FILE \
+                          --stack-name $STACK_NAME \
+                          --parameter-overrides StageName=$STAGE_NAME S3BucketName=$S3_BUCKET \
+                          --capabilities CAPABILITY_IAM
 
 export API_GATEWAY_URL=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[0].OutputValue' --output text)
 
